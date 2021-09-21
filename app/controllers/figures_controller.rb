9435115f512b5f622d157be4c7feec0503fd3f3e
amy-chinlai <amy@actionnetwork.org>
2021-09-21 12:35:09 -0400

    commit tree 9601e03fe85d0579671d4ee0b1f394bb44a301cd

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,56 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    if params[:title][:name].present?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if params[:landmark][:name].present?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    
+    @figure.save
+
+    redirect to "figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if params[:title][:name].present?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if params[:landmark][:name].present?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect to "/figures/#{@figure.id}"
+  end
 end
